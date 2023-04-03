bf57ab565b24f30501be4eb03f11b1b903993734
MAEDA Chikara <antoineblanot@gmail.com>
2023-04-03 11:16:33 +0900

    dataset build: choose to tokenize and prepare prompts

--- data/dataset.py
+++ data/dataset.py
@@ -7,15 +7,20 @@ from datasets import load_dataset
 
 class MultiNLIDataset(Dataset):
 
-    def __init__(self, split: str, tokenizer: AutoTokenizer) -> None:
+    def __init__(self, split: str, tokenizer: AutoTokenizer, do_tokenize: bool = True, do_prompt: bool = True) -> None:
         super().__init__()
         self.split = split
         self.tokenizer = tokenizer
         self.data = load_dataset("multi_nli", split=self.split)
         self.columns_to_keep = set(['input_ids', 'attention_mask', 'label'])
 
-        self.__prepare_prompt()
-        self.__tokenize()
+        if do_prompt:
+            self.__prepare_prompt()
+        else:
+            self.__prepare_inputs()
+
+        if do_tokenize:
+            self.__tokenize()
     
     def __len__(self):
         return len(self.data)
@@ -23,7 +28,7 @@ class MultiNLIDataset(Dataset):
     def __repr__(self) -> str:
         return self.data.__repr__()
     
-    def __prepare_prompt(self) -> str:
+    def __prepare_prompt(self) -> None:
         def prompt_function(examples):
             input_text = [
                 'instruction: {} {} premise: {} {} hypothesis: {}'.format(
@@ -36,6 +41,18 @@ class MultiNLIDataset(Dataset):
             prompt_function, batched=True, load_from_cache_file=False,
             desc='Preparing prompts for MNLI {} dataset'.format(self.split)
         )
+
+    def __prepare_inputs(self) -> None:
+        def inputs_function(examples):
+            input_text = [
+                '{} {} {}'.format(premise, self.tokenizer.sep_token, hypothesis)
+                for premise, hypothesis in zip(examples['premise'], examples['hypothesis'])
+            ]
+            return dict(input_text=input_text)
+        self.data = self.data.map(
+            inputs_function, batched=True, load_from_cache_file=False,
+            desc='Preparing inputs for MNLI {} dataset'.format(self.split)
+        )
     
     def __tokenize(self):
         preprocess_function = lambda examples: self.tokenizer(examples['input_text'], truncation=True)
@@ -43,6 +60,7 @@ class MultiNLIDataset(Dataset):
             preprocess_function, batched=True, load_from_cache_file=False,
             desc='Running tokenizer on MNLI {} dataset'.format(self.split)
         )
+        print(tokenized_data[0])
         self.data = tokenized_data.remove_columns(set(tokenized_data.column_names) - self.columns_to_keep)
     
     def __getitem__(self, index) -> dict:
