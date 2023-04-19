b3d7da8fb6260002547fbcbca0f33bcbb7935154
Antoine BLANOT <antoineblanot@gmail.com>
2023-04-19 15:54:28 +0900

    update roberta config

--- config/train/roberta.py
+++ config/train/roberta.py
@@ -2,25 +2,25 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.optim.lr_scheduler as lr_scheduler
 
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, get_polynomial_decay_schedule_with_warmup
 
 import model.modeling as models
 import data.dataset as datasets
 import data.preprocess as preprocesses
 import data.prompt as prompts
 
-NAME = 'roberta-base-mnli'
+NAME = 'roberta2-large-mnli'
 
 config = dict(
     name=NAME,
     model_cfg=dict(
         cls=models.RoBertaClassif,
-        name='roberta-base',
+        name='roberta-large',
         n_class=3
     ),
     tokenizer_cfg=dict(
         cls=AutoTokenizer.from_pretrained,
-        pretrained_model_name_or_path='roberta-base',
+        pretrained_model_name_or_path='roberta-large',
         model_max_length=128
     ),
     data_cfg=dict(
@@ -41,13 +41,13 @@ config = dict(
         ),
         scheduler=dict(
             cls=lr_scheduler.LambdaLR,
-            lr_lambda=lambda epoch: 1,
+            lr_lambda=lambda epoch: 1
         ),
         output_dir=f'exp/{NAME}',
         train_batch_size=32,
         eval_batch_size=32,
         device='cuda',
-        max_train_steps=12272*3,
+        max_train_steps=12387*3,
         eval_steps=1000,
         save_steps=1000,
         log_steps=1000,
